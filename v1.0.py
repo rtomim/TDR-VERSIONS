b2d0d7049a3bbd3eff6c31b93dc02429a703bdb3
Robert <rtomim05@gmail.com>
2022-09-29 16:43:23 +0200

    TDR-VERSIONS

---
+++ v1.0.py
@@ -0,0 +1,15 @@
+import arcade
+import sys
+import random
+import time
+
+WIDTH, HEIGHT = arcade.window_commands.get_display_size()
+
+
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(WIDTH, HEIGHT, 'Mr. Pelussy',)
+        self.name = ""
+        self.player_sprite = None
+        self.player_list = None
+
