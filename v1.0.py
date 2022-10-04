f256281673e046df497a77fcb670918e8bd9182b
Robert <rtomim05@gmail.com>
2022-10-04 16:38:32 +0200

    Personatge animat quan es mou horitzontalment.

--- v1.0.py
+++ v1.0.py
@@ -3,7 +3,62 @@ import arcade
 WIDTH, HEIGHT = arcade.window_commands.get_display_size()
 PLAYER_SCALING = 0.75
 MOVEMENT_SPEED = 6
+DRETA = 0
+ESQUERRA = 1
+ADALT = 1
+ABAIX = 0
 
+main_path = "Sprites_animacions/PELUSO_"
+
+
+def load_frame_pair(filename):
+    return [arcade.load_texture(filename),
+            arcade.load_texture(filename, flipped_horizontally=True)]
+
+
+class Protagonista(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__()
+
+        self.imatge_index = 0
+        self.direccio = 0
+        self.has_moved = False
+        self.standing_costat = load_frame_pair(f'{main_path}STANDING_COSTAT.png')
+
+        self.walk_frames_x = []
+        for x in range(8):
+            frame = load_frame_pair(f'{main_path}COSTAT_{x}.png')
+            self.walk_frames_x.append(frame)
+
+        self.walk_frames_y = []
+        for x in range(6):
+            frame = arcade.load_texture(f'{main_path}ADALT_{x}.png')
+            self.walk_frames_y.append(frame)
+            frame = arcade.load_texture(f'{main_path}ABAIX_{x}.png')
+            self.walk_frames_y.append(frame)
+
+        self.texture = self.walk_frames_y[1]
+
+    def update_animation(self, delta_time: float = 1 / 60):
+        if self.change_x > 0 and self.direccio != DRETA:
+            self.direccio = DRETA
+        if self.change_x < 0 and self.direccio != ESQUERRA:
+            self.direccio = ESQUERRA
+        if self.change_y > 0 and self.direccio != ADALT:
+            self.direccio = ADALT
+        if self.change_y < 0 and self.direccio != ABAIX:
+            self.direccio = ABAIX
+
+        if self.change_x != 0:
+            self.has_moved = True
+            self.imatge_index += 1
+            if self.imatge_index > 28:
+                self.imatge_index = 0
+            self.texture = self.walk_frames_x[self.imatge_index // 4][self.direccio]
+
+        if self.change_x == 0 and self.has_moved:
+            self.texture = self.standing_costat[self.direccio]
 
 
 class Game(arcade.Window):
@@ -16,7 +71,7 @@ class Game(arcade.Window):
 
     def setup(self):
         self.player_name = "Mr. Peluse"
-        self.player_sprite = arcade.Sprite('Sprites_animacions/PELUSO_ABAIX_0.png', PLAYER_SCALING)
+        self.player_sprite = Protagonista()
         self.player_sprite.center_x = WIDTH/2
         self.player_sprite.center_y = HEIGHT/2
         self.player_list = arcade.SpriteList()
@@ -67,7 +122,7 @@ class Game(arcade.Window):
         else:
             self.player_sprite.center_y += self.player_sprite.change_y
 
-        #self.player_sprite.update_animation()
+        self.player_sprite.update_animation()
 
 
 def main():
