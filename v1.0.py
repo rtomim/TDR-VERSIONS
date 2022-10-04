fd4f01e8cd2e7106d708a6f89d32681302d7971b
Robert <rtomim05@gmail.com>
2022-10-04 19:26:02 +0200

    Optimització controls, no diagonal, animacions verticals

--- v1.0.py
+++ v1.0.py
@@ -35,10 +35,11 @@ class Protagonista(arcade.Sprite):
         for x in range(6):
             frame = arcade.load_texture(f'{main_path}ADALT_{x}.png')
             self.walk_frames_y.append(frame)
+        for x in range(6):
             frame = arcade.load_texture(f'{main_path}ABAIX_{x}.png')
             self.walk_frames_y.append(frame)
 
-        self.texture = self.walk_frames_y[1]
+        self.texture = self.walk_frames_y[6]
 
     def update_animation(self, delta_time: float = 1 / 60):
         if self.change_x > 0 and self.direccio != DRETA:
@@ -57,8 +58,29 @@ class Protagonista(arcade.Sprite):
                 self.imatge_index = 0
             self.texture = self.walk_frames_x[self.imatge_index // 4][self.direccio]
 
-        if self.change_x == 0 and self.has_moved:
-            self.texture = self.standing_costat[self.direccio]
+        if self.change_y > 0:
+            self.has_moved = True
+            self.imatge_index += 1
+            if self.imatge_index > 15:
+                self.imatge_index = 0
+            self.texture = self.walk_frames_y[self.imatge_index // 3]
+
+        if self.change_y < 0:
+            self.has_moved = True
+            self.imatge_index += 1
+            if self.imatge_index > 15:
+                self.imatge_index = 0
+            self.texture = self.walk_frames_y[self.imatge_index // 3 + 6]
+
+        if self.change_x == 0 and self.change_y == 0 and self.has_moved:
+            if self.direccio == DRETA:
+                self.texture = self.standing_costat[DRETA]
+            if self.direccio == ESQUERRA:
+                self.texture = self.standing_costat[ESQUERRA]
+            if self.direccio == ADALT:
+                self.texture = self.walk_frames_y[0]
+            if self.direccio == ABAIX:
+                self.texture = self.walk_frames_y[6]
 
 
 class Game(arcade.Window):
@@ -68,6 +90,10 @@ class Game(arcade.Window):
         self.player_sprite = None
         self.player_list = None
         self.set_mouse_visible(False)
+        self.tecla_dreta = False
+        self.tecla_esquerra = False
+        self.tecla_abaix = False
+        self.tecla_adalt = False
 
     def setup(self):
         self.player_name = "Mr. Peluse"
@@ -79,13 +105,25 @@ class Game(arcade.Window):
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.RIGHT:
+            self.player_sprite.change_y = 0
             self.player_sprite.change_x = MOVEMENT_SPEED
+            self.tecla_dreta = True
+
         if key == arcade.key.LEFT:
+            self.player_sprite.change_y = 0
             self.player_sprite.change_x = -MOVEMENT_SPEED
+            self.tecla_esquerra = True
+
         if key == arcade.key.UP:
+            self.player_sprite.change_x = 0
             self.player_sprite.change_y = MOVEMENT_SPEED
+            self.tecla_adalt = True
+
         if key == arcade.key.DOWN:
+            self.player_sprite.change_x = 0
             self.player_sprite.change_y = -MOVEMENT_SPEED
+            self.tecla_abaix = True
+
         if key == arcade.key.ESCAPE:
             arcade.exit()
         if key == arcade.key.M:
@@ -93,23 +131,46 @@ class Game(arcade.Window):
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.RIGHT:
-            self.player_sprite.change_x = 0
+            self.tecla_dreta = False
+            if self.tecla_adalt:
+                self.player_sprite.change_y = MOVEMENT_SPEED
+            if self.tecla_abaix:
+                self.player_sprite.change_y = -MOVEMENT_SPEED
         if key == arcade.key.LEFT:
-            self.player_sprite.change_x = 0
+            self.tecla_esquerra = False
+            if self.tecla_adalt:
+                self.player_sprite.change_y = MOVEMENT_SPEED
+            if self.tecla_abaix:
+                self.player_sprite.change_y = -MOVEMENT_SPEED
         if key == arcade.key.UP:
-            self.player_sprite.change_y = 0
+            self.tecla_adalt = False
+            if self.tecla_dreta:
+                self.player_sprite.change_x = MOVEMENT_SPEED
+            if self.tecla_esquerra:
+                self.player_sprite.change_x = -MOVEMENT_SPEED
+
         if key == arcade.key.DOWN:
+            self.tecla_abaix = False
+            if self.tecla_dreta:
+                self.player_sprite.change_x = MOVEMENT_SPEED
+            if self.tecla_esquerra:
+                self.player_sprite.change_x = -MOVEMENT_SPEED
+
+        if not self.tecla_dreta and not self.tecla_esquerra:
+            self.player_sprite.change_x = 0
+
+        if not self.tecla_adalt and not self.tecla_abaix:
             self.player_sprite.change_y = 0
 
     def on_draw(self):
         arcade.start_render()
-        arcade.set_background_color((165, 140, 39))
+        arcade.set_background_color(arcade.color.WISTERIA)
         self.player_list.draw()
 
     def on_update(self, delta_time):
 
         if self.player_sprite.right + self.player_sprite.change_x > WIDTH:
-            self.player_sprite.right = WIDTH - 2
+            self.player_sprite.right = WIDTH
         elif self.player_sprite.left + self.player_sprite.change_x < 0:
             self.player_sprite.left = 2
         else:
@@ -121,6 +182,11 @@ class Game(arcade.Window):
             self.player_sprite.bottom = 0
         else:
             self.player_sprite.center_y += self.player_sprite.change_y
+
+        if self.player_sprite.change_x != 0:
+            self.player_sprite.change_y = 0
+        if self.player_sprite.change_y != 0:
+            self.player_sprite.change_x = 0
 
         self.player_sprite.update_animation()
 
